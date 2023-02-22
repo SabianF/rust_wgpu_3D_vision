db0af43eed85cba6ff9f13b757bccc0a8c14837b
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-21 21:55:20 -0800

    refactor: combined files to remove naming errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 
-use rust_wgpu_3D_vision::run;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window},
+};
+  
+#[cfg(target_arch="wasm32")]
+use wasm_bindgen::prelude::*;
+  
+fn run() {
+    env_logger::init();
+  
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    window.set_title("2D Object Renderer");
+  
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
+}
 
 fn main() {
     run();
