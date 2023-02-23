60544503c0e5be4dff8ea8bef0538a8fef113542
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-22 18:58:54 -0800

    refactor: mv fn to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+
+use winit::{
+  event::{Event, WindowEvent},
+  event_loop::{ControlFlow, EventLoop},
+  window::{Window},
+};
+
+#[cfg(target_arch="wasm32")]
+use wasm_bindgen::prelude::*;
+
+// ============================================================
+// Functions
+// ============================================================
+
+pub fn run() {
+  env_logger::init();
+
+  let event_loop = EventLoop::new();
+  let window = Window::new(&event_loop).unwrap();
+  window.set_title("2D Object Renderer");
+
+  event_loop.run(move |event, _, control_flow| {
+      *control_flow = ControlFlow::Wait;
+      match event {
+          Event::WindowEvent {
+              event: WindowEvent::CloseRequested,
+              ..
+          } => *control_flow = ControlFlow::Exit,
+          _ => {}
+      }
+  });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,5 @@
 
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::{Window},
-};
-  
-#[cfg(target_arch="wasm32")]
-use wasm_bindgen::prelude::*;
-  
-fn run() {
-    env_logger::init();
-  
-    let event_loop = EventLoop::new();
-    let window = Window::new(&event_loop).unwrap();
-    window.set_title("2D Object Renderer");
-  
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        }
-    });
-}
+use rust_wgpu_3D_vision::run;
 
 fn main() {
     run();
