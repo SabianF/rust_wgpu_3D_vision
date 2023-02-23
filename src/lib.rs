14eafb3600b8aea0aa5e5f381a187609323de737
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-22 19:56:08 -0800

    feat: created State.new() logic

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,73 @@ struct State {
 impl State {
 
   async fn new(window: Window) -> Self {
-    todo!()
+    /*
+     * The window size
+     */
+    let size = window.inner_size();
+
+    /*
+     * The handle to the GPU
+     */
+    let instance =wgpu::Instance::new(wgpu::InstanceDescriptor {
+      backends: wgpu::Backends::all(),
+      dx12_shader_compiler: Default::default(),
+    });
+
+    /*
+     * This ensures the surface only lives as long as its parent window
+     */
+    let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+    let adapter = instance.request_adapter(
+      &wgpu::RequestAdapterOptions {
+        power_preference      : wgpu::PowerPreference::default(),
+        compatible_surface    : Some(&surface),
+        force_fallback_adapter: false,
+      },
+    ).await.unwrap();
+
+    let (device, queue) = adapter.request_device(
+      &wgpu::DeviceDescriptor {
+        features: wgpu::Features::empty(),
+        limits  : if cfg!(target_arch = "wasm32") {
+          wgpu::Limits::downlevel_webgl2_defaults()
+        } else {
+          wgpu::Limits::default()
+        },
+        label: None,
+      },
+      None,
+    ).await.unwrap();
+
+    let surface_caps = surface.get_capabilities(&adapter);
+
+    let surface_format = surface_caps.formats.iter()
+      .copied()
+      .filter(|f| f.describe().srgb)
+      .next()
+      .unwrap_or(surface_caps.formats[0]);
+
+    let config = wgpu::SurfaceConfiguration {
+        usage       : wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format      : surface_format,
+        width       : size.width,
+        height      : size.height,
+        present_mode: surface_caps.present_modes[0],
+        alpha_mode: surface_caps.alpha_modes[0],
+        view_formats: vec![],
+    };
+
+    surface.configure(&device, &config);
+
+    Self {
+      window,
+      surface,
+      device,
+      queue,
+      config,
+      size,
+    }
   }
 
   pub fn window(&self) -> &Window {
