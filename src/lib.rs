ce68dc8a2ff192090f188803c2d38bc729516483
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-22 19:04:29 -0800

    feat: added State type template

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,49 @@ use winit::{
 use wasm_bindgen::prelude::*;
 
 // ============================================================
+// Types
+// ============================================================
+
+
+struct State {
+  surface : wgpu::Surface,
+  device  : wgpu::Device,
+  queue   : wgpu::Queue,
+  config  : wgpu::SurfaceConfiguration,
+  size    : winit::dpi::PhysicalSize<u32>,
+  window  : Window,
+}
+
+// ============================================================
 // Functions
 // ============================================================
+
+impl State {
+
+  async fn new(window: Window) -> Self {
+    todo!()
+  }
+
+  pub fn window(&self) -> &Window {
+      &self.window
+  }
+
+  fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+      todo!()
+  }
+
+  fn input(&mut self, event: &WindowEvent) -> bool {
+      todo!()
+  }
+
+  fn update(&mut self) {
+      todo!()
+  }
+
+  fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+      todo!()
+  }
+}
 
 pub fn run() {
   env_logger::init();
