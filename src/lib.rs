93f3ad42ea52b5713723b7eed49beac66c95aa25
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-25 14:19:49 -0800

    fix: device crash, out of memory during resize
    This forces the State to use a fallback adapter, and is not a
    sustainable solution, as it limits the adapter to a software simulated
    implementation

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@ impl State {
       &wgpu::RequestAdapterOptions {
         power_preference      : wgpu::PowerPreference::default(),
         compatible_surface    : Some(&surface),
-        force_fallback_adapter: false,
+        force_fallback_adapter: true,
       },
     ).await.unwrap();
 
