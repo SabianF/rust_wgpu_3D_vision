ad066599d1c539dd1ce8ff6e829685ac643bc246
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-25 20:43:42 -0800

    feat: render triangle on screen

--- src/lib.rs
+++ src/lib.rs
@@ -209,7 +209,7 @@ impl State {
     // we need to use a scoped block to release this mutable borrow, to call
     // encoder.finish()
     {
-      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+      let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
         label: Some("Render Pass"),
 
         color_attachments: &[Some(wgpu::RenderPassColorAttachment {
@@ -230,6 +230,9 @@ impl State {
 
         depth_stencil_attachment: None,
       });
+
+      render_pass.set_pipeline(&self.render_pipeline);
+      render_pass.draw(0..3, 0..1);
     }
 
     self.queue.submit(std::iter::once(encoder.finish()));
