82f1b9174439e34486340d4c81700acda759ea0c
SabianF <58588133+SabianF@users.noreply.github.com>
2023-03-02 15:54:15 -0800

    refactor: mv fns

--- src/lib.rs
+++ src/lib.rs
@@ -465,6 +465,70 @@ impl State {
   }
 }
 
+pub async fn run() {
+  env_logger::init();
+
+  let event_loop = EventLoop::new();
+
+  let window = Window::new(&event_loop).unwrap();
+  window.set_title("2D Object Renderer");
+
+  let mut state = State::new(window).await;
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+    match event {
+      Event::WindowEvent {
+        ref event,
+        window_id,
+      } if window_id == state.window().id() => {
+        if !state.input(event) {
+          match event {
+
+            WindowEvent::CloseRequested => {
+              *control_flow = ControlFlow::Exit;
+            },
+
+            WindowEvent::Resized(physical_size) => {
+              state.resize(*physical_size);
+            },
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+              state.resize(**new_inner_size);
+            },
+
+            _ => {}
+          }
+        }
+      }
+
+      // Rendering the surface
+      Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+        state.update();
+        match state.render() {
+          Ok(_) => {}
+
+          // Reconfigure the surface if lost
+          Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+
+          // Prevent excess memory usage
+          Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+
+          // Handle all other errors
+          Err(e) => eprintln!("{:?}", e),
+        }
+      }
+
+      Event::MainEventsCleared => {
+        // Ensure RedrawRequested triggers only once, unless manually
+        // requested.
+        state.window().request_redraw();
+      }
+
+      _ => {}
+    }
+  });
+}
+
 fn configure_instances(device: &wgpu::Device) -> (
   Vec<Instance>,
   wgpu::Buffer,
@@ -794,68 +858,4 @@ fn configure_camera(
     camera_bind_group,
     camera_controller,
   )
-}
-
-pub async fn run() {
-  env_logger::init();
-
-  let event_loop = EventLoop::new();
-
-  let window = Window::new(&event_loop).unwrap();
-  window.set_title("2D Object Renderer");
-
-  let mut state = State::new(window).await;
-
-  event_loop.run(move |event, _, control_flow| {
-    *control_flow = ControlFlow::Wait;
-    match event {
-      Event::WindowEvent {
-        ref event,
-        window_id,
-      } if window_id == state.window().id() => {
-        if !state.input(event) {
-          match event {
-
-            WindowEvent::CloseRequested => {
-              *control_flow = ControlFlow::Exit;
-            },
-
-            WindowEvent::Resized(physical_size) => {
-              state.resize(*physical_size);
-            },
-            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-              state.resize(**new_inner_size);
-            },
-
-            _ => {}
-          }
-        }
-      }
-
-      // Rendering the surface
-      Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-        state.update();
-        match state.render() {
-          Ok(_) => {}
-
-          // Reconfigure the surface if lost
-          Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
-
-          // Prevent excess memory usage
-          Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-
-          // Handle all other errors
-          Err(e) => eprintln!("{:?}", e),
-        }
-      }
-
-      Event::MainEventsCleared => {
-        // Ensure RedrawRequested triggers only once, unless manually
-        // requested.
-        state.window().request_redraw();
-      }
-
-      _ => {}
-    }
-  });
 }
