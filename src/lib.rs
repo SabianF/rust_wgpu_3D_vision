164d520b4d0ac4c9ac481ddcfc9a05938c8e04fc
SabianF <58588133+SabianF@users.noreply.github.com>
2023-03-02 15:31:48 -0800

    refactor: mv window, device, surface config to fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-use wgpu::util::DeviceExt;
+use wgpu::{util::DeviceExt, SurfaceConfiguration};
 use winit::{
   event::{Event, WindowEvent, KeyboardInput, ElementState, VirtualKeyCode},
   event_loop::{ControlFlow, EventLoop},
@@ -317,64 +317,13 @@ impl State {
 
   async fn new(window: Window) -> Self {
 
-    /*
-     * The window size
-     */
-    let size = window.inner_size();
-
-    /*
-     * The handle to the GPU
-     */
-    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-      backends: wgpu::Backends::all(),
-      dx12_shader_compiler: Default::default(),
-    });
-
-    /*
-     * This ensures the surface only lives as long as its parent window
-     */
-    let surface = unsafe { instance.create_surface(&window) }.unwrap();
-
-    let adapter = instance.request_adapter(
-      &wgpu::RequestAdapterOptions {
-        power_preference      : wgpu::PowerPreference::default(),
-        compatible_surface    : Some(&surface),
-        force_fallback_adapter: true,
-      },
-    ).await.unwrap();
-
-    let (device, queue) = adapter.request_device(
-      &wgpu::DeviceDescriptor {
-        features: wgpu::Features::empty(),
-        limits  : if cfg!(target_arch = "wasm32") {
-          wgpu::Limits::downlevel_webgl2_defaults()
-        } else {
-          wgpu::Limits::default()
-        },
-        label: None,
-      },
-      None,
-    ).await.unwrap();
-
-    let surface_caps = surface.get_capabilities(&adapter);
-
-    let surface_format = surface_caps.formats.iter()
-      .copied()
-      .filter(|f| f.describe().srgb)
-      .next()
-      .unwrap_or(surface_caps.formats[0]);
-
-    let config = wgpu::SurfaceConfiguration {
-      usage       : wgpu::TextureUsages::RENDER_ATTACHMENT,
-      format      : surface_format,
-      width       : size.width,
-      height      : size.height,
-      present_mode: surface_caps.present_modes[0],
-      alpha_mode: surface_caps.alpha_modes[0],
-      view_formats: vec![],
-    };
-
-    surface.configure(&device, &config);
+    let (
+      size,
+      surface,
+      device,
+      queue,
+      config
+    ) = configure_surface(&window).await;
 
     let camera = Camera {
       // position the camera one unit up and 2 units back
@@ -688,6 +637,77 @@ impl State {
     output.present();
     return Ok(());
   }
+}
+
+/**
+ * Configures the rendering window and surface
+ */
+async fn configure_surface(window: &Window) -> (
+  winit::dpi::PhysicalSize<u32>,
+  wgpu::Surface,
+  wgpu::Device,
+  wgpu::Queue,
+  SurfaceConfiguration,
+) {
+    /*
+     * The window size
+     */
+    let size = window.inner_size();
+
+    /*
+     * The handle to the GPU
+     */
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+      backends: wgpu::Backends::all(),
+      dx12_shader_compiler: Default::default(),
+    });
+
+    /*
+     * This ensures the surface only lives as long as its parent window
+     */
+    let surface = unsafe { instance.create_surface(window) }.unwrap();
+
+    let adapter = instance.request_adapter(
+      &wgpu::RequestAdapterOptions {
+        power_preference      : wgpu::PowerPreference::default(),
+        compatible_surface    : Some(&surface),
+        force_fallback_adapter: true,
+      },
+    ).await.unwrap();
+
+    let (device, queue) = adapter.request_device(
+      &wgpu::DeviceDescriptor {
+        features: wgpu::Features::empty(),
+        limits  : if cfg!(target_arch = "wasm32") {
+          wgpu::Limits::downlevel_webgl2_defaults()
+        } else {
+          wgpu::Limits::default()
+        },
+        label: None,
+      },
+      None,
+    ).await.unwrap();
+
+    let surface_caps = surface.get_capabilities(&adapter);
+
+    let surface_format = surface_caps.formats.iter()
+      .copied()
+      .filter(|f| f.describe().srgb)
+      .next()
+      .unwrap_or(surface_caps.formats[0]);
+
+    let config = wgpu::SurfaceConfiguration {
+      usage       : wgpu::TextureUsages::RENDER_ATTACHMENT,
+      format      : surface_format,
+      width       : size.width,
+      height      : size.height,
+      present_mode: surface_caps.present_modes[0],
+      alpha_mode: surface_caps.alpha_modes[0],
+      view_formats: vec![],
+    };
+
+    surface.configure(&device, &config);
+    return (size, surface, device, queue, config);
 }
 
 pub async fn run() {
