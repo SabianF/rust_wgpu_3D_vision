6f8768cd22833f7593aff7373a49c8f2a4580d5e
SabianF <58588133+SabianF@users.noreply.github.com>
2023-03-02 15:23:48 -0800

    feat: render multiple instances

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use winit::{
   event_loop::{ControlFlow, EventLoop},
   window::{Window},
 };
+use cgmath::prelude::*;
 
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
@@ -49,6 +50,8 @@ struct State {
   camera_buffer     : wgpu::Buffer,
   camera_bind_group : wgpu::BindGroup,
   camera_controller : CameraController,
+  instances         : Vec<Instance>,
+  instance_buffer   : wgpu::Buffer,
 }
 
 // This is needed to store data correctly for the shaders
@@ -66,6 +69,24 @@ struct CameraUniform {
 struct Vertex {
   position: [f32; 3], // [X, Y, Z]
   color   : [f32; 3], // [R, G, B]
+}
+
+/**
+ * Defines the properties of different instances of objects/models
+ */
+struct Instance {
+  position: cgmath::Vector3<f32>,
+  rotation: cgmath::Quaternion<f32>,
+}
+
+/**
+ * A workaround to use quaternions in WGSL, since WGSL doesn't use quaternions
+ * We're using this format to easily input rotation data into the Buffer
+ */
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct InstanceRaw {
+    model: [[f32; 4]; 4],
 }
 
 #[rustfmt::skip]
@@ -112,6 +133,13 @@ const CUBE_INDICES: &[u16] = &[
   6, 5, 4,
   4, 7, 6,
 ];
+
+const NUM_INSTANCES_PER_ROW: u32 = 10;
+const INSTANCE_SPACING: cgmath::Vector3<f32> = cgmath::Vector3::new(
+  NUM_INSTANCES_PER_ROW as f32 * 0.5,
+  0.0,
+  NUM_INSTANCES_PER_ROW as f32 * 0.5,
+);
 
 // ============================================================
 // Functions
@@ -233,6 +261,57 @@ impl Vertex {
       array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
       step_mode   : wgpu::VertexStepMode::Vertex,
       attributes  : &Self::ATTRIBUTES,
+    }
+  }
+}
+
+impl Instance {
+  fn to_raw(&self) -> InstanceRaw {
+    InstanceRaw {
+      model: (
+        cgmath::Matrix4::from_translation(self.position)
+        * cgmath::Matrix4::from(self.rotation)
+      ).into(),
+    }
+  }
+}
+
+impl InstanceRaw {
+  fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+    use std::mem;
+    wgpu::VertexBufferLayout {
+      array_stride: mem::size_of::<InstanceRaw>() as wgpu::BufferAddress,
+      // We need to switch from using a step mode of Vertex to Instance
+      // This means that our shaders will only change to use the next
+      // instance when the shader starts processing a new instance
+      step_mode: wgpu::VertexStepMode::Instance,
+      attributes: &[
+        wgpu::VertexAttribute {
+          offset: 0,
+          // While our vertex shader only uses locations 0, and 1 now, in later tutorials we'll
+          // be using 2, 3, and 4, for Vertex. We'll start at slot 5 not conflict with them later
+          shader_location: 5,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+        // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
+        // for each vec4. We'll have to reassemble the mat4 in
+        // the shader.
+        wgpu::VertexAttribute {
+          offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
+          shader_location: 6,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+        wgpu::VertexAttribute {
+          offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+          shader_location: 7,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+        wgpu::VertexAttribute {
+          offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
+          shader_location: 8,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+      ],
     }
   }
 }
@@ -381,7 +460,10 @@ impl State {
           // This tells `wgpu` what type of vertices to pass to the
           // vertex shader. Since we're specifying the vertices in the vertex
           // shader itself, empty is OK
-          buffers     : &[ Vertex::desc(), ],
+          buffers: &[
+            Vertex::desc(),
+            InstanceRaw::desc(),
+          ],
         },
 
         // Stores color data to the `surface`
@@ -455,6 +537,50 @@ impl State {
 
     let camera_controller = CameraController::new(0.2);
 
+    let instances = (0..NUM_INSTANCES_PER_ROW)
+      .flat_map(|z| {
+        (0..NUM_INSTANCES_PER_ROW).map(move |x| {
+          let position = cgmath::Vector3 {
+            x: x as f32,
+            y: 0.0,
+            z: z as f32
+          } - INSTANCE_SPACING;
+
+          let rotation = if position.is_zero() {
+            // this is needed so an object at (0, 0, 0) won't get scaled to zero
+            // as Quaternions can effect scale if they're not created correctly
+            cgmath::Quaternion::from_axis_angle(
+              cgmath::Vector3::unit_z(),
+              cgmath::Deg(0.0)
+            )
+          } else {
+            cgmath::Quaternion::from_axis_angle(
+              position.normalize(),
+              cgmath::Deg(45.0)
+            )
+          };
+
+          return Instance {
+            position,
+            rotation,
+          };
+        })
+      })
+      .collect::<Vec<_>>();
+
+    let instance_data = instances
+      .iter()
+      .map(Instance::to_raw)
+      .collect::<Vec<_>>();
+
+    let instance_buffer = device.create_buffer_init(
+      &wgpu::util::BufferInitDescriptor {
+        label   : Some("Instance buffer"),
+        contents: bytemuck::cast_slice(&instance_data),
+        usage   : wgpu::BufferUsages::VERTEX,
+      },
+    );
+
     Self {
       window,
       surface,
@@ -472,6 +598,8 @@ impl State {
       camera_buffer,
       camera_bind_group,
       camera_controller,
+      instances,
+      instance_buffer,
     }
   }
 
@@ -547,6 +675,10 @@ impl State {
         0,
         self.cube_vertex_buffer.slice(..),
       );
+      render_pass.set_vertex_buffer(
+        1,
+        self.instance_buffer.slice(..),
+      );
       render_pass.set_index_buffer(
         self.cube_index_buffer.slice(..),
         wgpu::IndexFormat::Uint16,
@@ -554,7 +686,7 @@ impl State {
       render_pass.draw_indexed(
         0..self.cube_indices_count,
         0,
-        0..1,
+        0..self.instances.len() as _,
       );
     }
 
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -8,6 +8,13 @@ struct VertexInput {
   @location(1) color: vec3<f32>,
 };
 
+struct InstanceInput {
+  @location(5) model_matrix_0: vec4<f32>,
+  @location(6) model_matrix_1: vec4<f32>,
+  @location(7) model_matrix_2: vec4<f32>,
+  @location(8) model_matrix_3: vec4<f32>,
+};
+
 struct VertexOutput {
   @builtin(position) clip_position: vec4<f32>,
   @location(0) color: vec3<f32>,
@@ -17,10 +24,27 @@ struct VertexOutput {
 var<uniform> camera: CameraUniform;
 
 @vertex
-fn vs_main(model: VertexInput) -> VertexOutput {
+fn vs_main(
+  model   : VertexInput,
+  instance: InstanceInput,
+) -> VertexOutput {
+
+  let model_matrix = mat4x4<f32>(
+    instance.model_matrix_0,
+    instance.model_matrix_1,
+    instance.model_matrix_2,
+    instance.model_matrix_3,
+  );
+
   var output: VertexOutput;
-  output.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
   output.color = model.color;
+
+  output.clip_position = (
+    camera.view_proj
+      * model_matrix
+      * vec4<f32>(model.position, 1.0)
+  );
+
   return output;
 }
 
